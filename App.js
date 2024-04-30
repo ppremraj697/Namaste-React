f02efb35bfa88dd5e2e389ea52ea37ecc320da34
Premraj Patil <ppremraj697@gmail.com>
2024-04-30 22:49:08 +0530

    Understanding JSX fundamentals

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-const heading1 = React.createElement("h1", { id: "heading", xyz: "abc" }, "Hello World from React!!!")
+// React.createElement => gives us ReactElement(JS Object) => rendered as HTMLElement
+const heading1 = React.createElement("h1", { id: "heading", xyz: "abc" }, "Hello World from React!!!");//React element using core React
 
 const root1 = ReactDOM.createRoot(document.getElementById("root1"))
 
@@ -10,6 +11,8 @@ console.log("typeof heading1", typeof heading1)
 console.log(typeof root1)
 
 root1.render(heading1);
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 /*
 Creating nested react element similar to this using React.createElement() method
@@ -35,3 +38,19 @@ const parent = React.createElement(
 const nestedReactElement = ReactDOM.createRoot(document.getElementById("nestedReactElement"))
 
 nestedReactElement.render(parent);
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+// JSX => Extension of JS language for writing HTML-like markup inside a JS file.
+//JSX is not a part of React && JSX is not pure JS
+// jsx is syntax and react element is object
+
+// JSX (it is transpiled before it reaches JS engine and is done by Parcel and Babel)
+
+//JSX => transpiled into React.createElement => gives us ReactElement(JS Object) => rendered as HTMLElement
+const jsxHeading = (<h1 id="heading" className="jsxRoot">Namaste React using JSX</h1>);//React element using JSX
+console.log(jsxHeading)
+console.log("typeof JSX tag: ", typeof jsxHeading);
+
+const jsxRoot = ReactDOM.createRoot(document.getElementById("jsxRoot"))
+
+jsxRoot.render(jsxHeading) 
