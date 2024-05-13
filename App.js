b16d8cb2793054bc0d5ddfcb18f3252c92f4d5eb
Premraj Patil <ppremraj697@gmail.com>
2024-05-13 19:08:30 +0530

    React Functional Component and Fun with JSX

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-// React.createElement => gives us ReactElement(JS Object) => rendered as HTMLElement
+// React.createElement => gives us ReactElement(JS Object) => rendered as HTMLElement on DOM
 const heading1 = React.createElement("h1", { id: "heading", xyz: "abc" }, "Hello World from React!!!");//React element using core React
 
 const root1 = ReactDOM.createRoot(document.getElementById("root1"))
@@ -44,13 +44,44 @@ nestedReactElement.render(parent);
 //JSX is not a part of React && JSX is not pure JS
 // jsx is syntax and react element is object
 
-// JSX (it is transpiled before it reaches JS engine and is done by Parcel and Babel)
+// JSX (it is transpiled before it reaches JS engine and is done by Parcel with the help of Babel)
 
-//JSX => transpiled into React.createElement => gives us ReactElement(JS Object) => rendered as HTMLElement
+//JSX => transpiled into React.createElement => gives us ReactElement(JS Object) => rendered as HTMLElement on DOM
 const jsxHeading = (<h1 id="heading" className="jsxRoot">Namaste React using JSX</h1>);//React element using JSX
 console.log(jsxHeading)
 console.log("typeof JSX tag: ", typeof jsxHeading);
 
 const jsxRoot = ReactDOM.createRoot(document.getElementById("jsxRoot"))
 
-jsxRoot.render(jsxHeading) 
+jsxRoot.render(jsxHeading)
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+// React Functional Component(NEW) is just a normal JS function that returns some piece of JSX or React Element
+
+// const HeadingComponent = () => {
+//     return <h1>Namaste React's First Functional Component</h1>;
+// }
+
+const Title = () => (
+    <h1>Title Component</h1>
+);
+
+const num = 109283746;
+
+const HeadingComponent = () => (
+    <div id="container">
+        <Title></Title>
+        <Title />{/* Rendering React Component inside React Component */}
+        {Title()}
+        {/* Execute any JS Expression/Code inside JSX */}
+        <h2>{num}</h2>
+        {jsxHeading/*Rendering React Element inside React Component */}
+        <h1 id="heading">Namaste React's First Functional Component</h1>
+    </div>
+);
+
+const componentRoot = ReactDOM.createRoot(document.getElementById("functionalComponentRoot"))
+componentRoot.render(<HeadingComponent />)
+
+//Component Composition == Nesting of Component
+//JSX takes care of Injection Attacks - Prevents cross site scripting 
