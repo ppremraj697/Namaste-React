275c7286d6647e0584789926959795713ab41a73
Premraj Patil <ppremraj697@gmail.com>
2024-06-03 23:51:18 +0530

    Implemented top-rated-restaurants feature using React Hook - useState()

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,27 @@
 import RestaurantCard from "./RestaurantCard";
 import { restaurantData } from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+
+    const [restaurantList, setRestaurantList] = useState(restaurantData);
+
     return (
         <div className="body">
-            <div className="search">Search</div>
+            <div className="filter">
+                <button
+                    className="top-rated-restaurant btn"
+                    onClick={() => {
+                        const topRatedRestaurants = restaurantList.filter(
+                            (res) => res.info.avgRating > 4.1
+                        );
+                        setRestaurantList(topRatedRestaurants)
+                    }}>Top Rated Restaurants
+                </button>
+            </div>
             <div className="restaurant-container">
                 {
-                    restaurantData.map((restaurant) => <RestaurantCard restaurantData={restaurant} key={restaurant.info.id} />)
+                    restaurantList.map((restaurant) => <RestaurantCard restaurantData={restaurant} key={restaurant.info.id} />)
                 }
             </div>
         </div>
