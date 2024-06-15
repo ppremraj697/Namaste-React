764db85a3cedd22c83ae5c1df527fd6ea63a9778
Premraj Patil <ppremraj697@gmail.com>
2024-06-15 23:25:53 +0530

    Implemented 'Search restaurant with name' feature

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,6 +6,11 @@ import Shimmer from "./Shimmer";
 const Body = () => {
 
     const [restaurantList, setRestaurantList] = useState([]);
+    const [filteredRestaurantList, setFilteredRestaurantList] = useState([]);
+    const [searchText, setSearchText] = useState("");
+
+    //Whenever state variables update, React triggers a reconciliation cycle(re-renders the component).
+    console.log("Body Rendered");
 
     useEffect(() => {
         fetchData();
@@ -17,6 +22,8 @@ const Body = () => {
         console.log(json);
         //Optional Chaining
         setRestaurantList(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+        setFilteredRestaurantList(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+        console.log("restaurant list", restaurantList)
     }
 
     //Conditional Rendering
@@ -27,6 +34,24 @@ const Body = () => {
     return (
         <div className="body">
             <div className="filter">
+                <div className="search">
+                    <input
+                        type="text"
+                        className="search-box"
+                        value={searchText}
+                        onChange={(e) => {
+                            setSearchText(e.target.value);
+                        }}
+                    />
+                    <button
+                        className="search-btn"
+                        onClick={() => {
+                            console.log(searchText)
+                            const searchedRestaurants = restaurantList.filter((res) => res.info.name.toLowerCase().includes(searchText.toLowerCase()));
+                            setFilteredRestaurantList(searchedRestaurants);
+                        }}>Search
+                    </button>
+                </div>
                 <button
                     className="top-rated-restaurant btn"
                     onClick={() => {
@@ -38,9 +63,7 @@ const Body = () => {
                 </button>
             </div>
             <div className="restaurant-container">
-                {
-                    restaurantList.map((restaurant) => <RestaurantCard restaurantData={restaurant} key={restaurant.info.id} />)
-                }
+                {filteredRestaurantList.map((restaurant) => <RestaurantCard restaurantData={restaurant} key={restaurant.info.id} />)}
             </div>
         </div>
     )
